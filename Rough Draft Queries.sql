e5e7dce3c8cbf846370b6c8b43b9293ee71b8025
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-09-28 02:38:15 -0700

    Add files via upload

---
+++ Rough Draft Queries.sql
@@ -0,0 +1,22 @@
+ 
+
+select * from sakila.attacks ;
+
+SELECT * FROM sakila.attacks  order by Country asc;
+
+
+select * from sakila.attacks  where Country = '' OR Country IS NULL;
+
+/*
+​What body parts are most often injured? (hint: you'll need to perform text analysis on the Injury column)
+*/
+
+
+ 
+
+
+
+ 
+
+
+ 
