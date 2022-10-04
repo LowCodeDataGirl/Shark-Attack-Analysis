2170a37083676986c9746e01cc999aaffd6a1874
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-10-04 04:45:23 -0700

    Update Rough Draft Queries.sql

--- Rough Draft Queries.sql
+++ Rough Draft Queries.sql
@@ -21,7 +21,131 @@ select * from sakila.attacks  where Country = '' OR Country IS NULL;
 
 /*
 ​What body parts are most often injured? (hint: you'll need to perform text analysis on the Injury column)
-*/
+*/ /* Some Columns in the dataset contains irrelavant additional words '?'. 
+ This query checks for all the columns that contain "?"*/
+ 
+SELECT  Country,                   REPLACE( Country , '?', ' '),
+        Injury,                    REPLACE(Injury, '?', ' '),
+	    `Case Number`,             REPLACE(`Case Number` , '?', ' '),
+        `Date`,                    REPLACE(`Date` , '?', ' '),
+        `Year`,                    REPLACE(`Year` , '?', ' '),
+        `Type`,                    REPLACE(`Type` , '?', ' '),
+        `Country`,                 REPLACE(`Country` , '?', ' '),
+        `Area`,                    REPLACE(`Area` , '?', ' '),
+        `Location`,                REPLACE(`Location` , '?', ' '),
+        `Activity`,                REPLACE(`Activity` , '?', ' '),
+        `Name`,                    REPLACE(`Name` , '?', ' '),
+        `Sex`,                     REPLACE(`Sex`, '?', ' '),
+        `Age`,                     REPLACE(`Age` , '?', ' '),
+        `Injury`,                  REPLACE(`Injury`, '?', ' '),
+        `Fatal (Y/N)`,             REPLACE(`Fatal (Y/N)` , '?', ' '),
+        `Time`,                    REPLACE(`Time` , '?', ' '),
+        `Species`,                 REPLACE(`Species` , '?', ' '),
+        `Investigator or Source`,  REPLACE(`Investigator or Source` , '?', ' '),
+        `pdf`,                     REPLACE(`pdf` , '?', ' '),
+        `href formula`,            REPLACE(`href formula` , '?', ' '),
+        `href`,                    REPLACE(`href` , '?', ' '),
+        `Case Number_[0]`,         REPLACE(`Case Number_[0]` , '?', ' '),
+        `original order`,          REPLACE(`original order` , '?', ' ')
+        
+FROM    sakila.attacks 
+WHERE   Injury LIKE '%?%'
+OR      Country LIKE '%?%'
+OR      `Date` LIKE '%?%'
+OR      Location LIKE '%?%'
+OR      Species LIKE '%?%'
+OR      `Age` LIKE '%?%'
+OR      `Time` LIKE '%?%';
+ 
+
+ 
+UPDATE sakila.attacks 
+SET    Injury    = REPLACE(Injury, '?', ' '),
+	   Country   = REPLACE(Country , '?', ' '),
+      `Date`     = REPLACE(`Date` , '?', ' '),
+       Location  = REPLACE(Location , '?', ' '),
+       Species   = REPLACE(Species , '?', ' '),
+       `Age`     = REPLACE(`Age` , '?', ' '),
+      `Time`     = REPLACE(`Time` , '?', ' ')
+WHERE Injury   LIKE '%?%'
+OR    Country  LIKE '%?%'
+OR   `Date`    LIKE '%?%'
+OR   Location  LIKE '%?%'
+OR   Species   LIKE '%?%'
+OR   `Age`     LIKE '%?%'
+OR   `Time`    LIKE '%?%';
+
+
+
+
+
+UPDATE sakila.attacks 
+SET
+
+        Country=                   REPLACE( Country , '?', ' '),
+        Injury=                  REPLACE(Injury, '?', ' '),
+	    `Case Number`=             REPLACE(`Case Number` , '?', ' '),
+        `Date`=                 REPLACE(`Date` , '?', ' '),
+        `Year`=                   REPLACE(`Year` , '?', ' '),
+        `Type`=                   REPLACE(`Type` , '?', ' '),
+        `Country`=               REPLACE(`Country` , '?', ' '),
+        `Area`=                  REPLACE(`Area` , '?', ' '),
+        `Location`=               REPLACE(`Location` , '?', ' '),
+        `Activity`=              REPLACE(`Activity` , '?', ' '),
+        `Name`=                   REPLACE(`Name` , '?', ' '),
+        `Sex`=                   REPLACE(`Sex`, '?', ' '),
+        `Age`=                   REPLACE(`Age` , '?', ' '),
+        `Injury`=                  REPLACE(`Injury`, '?', ' '),
+        `Fatal (Y/N)`=           REPLACE(`Fatal (Y/N)` , '?', ' '),
+        `Time`=                   REPLACE(`Time` , '?', ' '),
+        `Species`=                REPLACE(`Species` , '?', ' '),
+        `Investigator or Source`=  REPLACE(`Investigator or Source` , '?', ' '),
+        `pdf`=          REPLACE(`pdf` , '?', ' '),
+        `href formula`=           REPLACE(`href formula` , '?', ' '),
+        `href`=                  REPLACE(`href` , '?', ' '),
+        `Case Number_[0]`=         REPLACE(`Case Number_[0]` , '?', ' '),
+        `original order`=        REPLACE(`original order` , '?', ' ')
+        
+WHERE Injury   LIKE '%?%'
+OR  Country LIKE '%?%'
+    OR     Injury LIKE '%?%'
+	OR     `Case Number`LIKE '%?%'
+    OR      `Date`LIKE '%?%'
+    OR      `Year`LIKE '%?%'
+    OR     `Type`LIKE '%?%'
+    OR     `Country`LIKE '%?%'
+    OR     `Area`LIKE '%?%'
+    OR      `Location`LIKE '%?%'
+    OR      `Activity`LIKE '%?%'
+    OR      `Name`LIKE '%?%'
+    OR     `Sex`LIKE '%?%'
+    OR        `Age`LIKE '%?%'
+    OR       `Injury`LIKE '%?%'
+    OR      `Fatal (Y/N)`LIKE '%?%'
+    OR      `Time`LIKE '%?%'
+    OR      `Species`LIKE '%?%'
+    OR      `Investigator or Source`LIKE '%?%'
+	OR     `pdf`LIKE '%?%'
+    OR      `href formula`LIKE '%?%'
+	OR     `href`LIKE '%?%'
+	OR     `Case Number_[0]`LIKE '%?%'
+    OR   `original order`LIKE '%?%'
+        
+
+
+
+
+
+
+OR    Country  LIKE '%?%'
+OR   `Date`    LIKE '%?%'
+OR   Location  LIKE '%?%'
+OR   Species   LIKE '%?%'
+OR   `Age`     LIKE '%?%'
+OR   `Time`    LIKE '%?%';
+
+
+
 
 
  
