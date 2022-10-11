cd90d43ba932b1ffa3f01c66bccd6498449d46e6
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-10-11 16:53:01 -0700

    Update Rough Draft Queries.sql

--- Rough Draft Queries.sql
+++ Rough Draft Queries.sql
@@ -5,3 +5,21 @@ TO DO LIST
 2. Finish the like sharkattacks data cleaning 
 3. Check for href with double http
 4. Check each column one by one 
+
+*/
+
+SELECT  *
+FROM sharkattacks.attacks 
+where `Case Number_[1]` like '%sharkattack%';
+
+
+SELECT  *
+FROM sharkattacks.attacks 
+where `Case Number` ='1758.00.00' ;
+
+
+
+  
+
+
+
