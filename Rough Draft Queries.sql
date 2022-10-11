144a8aa19bf11c2cc111a6ff2c475f1232978664
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-10-11 15:57:05 -0700

    Update Rough Draft Queries.sql

--- Rough Draft Queries.sql
+++ Rough Draft Queries.sql
@@ -1,21 +1,7 @@
-, Year, Country, Area, Activity, Sex, Age, Injury, Fatal (Y/N), Time, Species, Investigator or Source, pdf, href formula, href, Case Number_[0], Case Number_[1], original order
+/* 
 
-SELECT  *
-FROM sakila.attacks 
-order by  cast( `original order` as decimal) asc;
-
-SELECT distinct `Case Number`, `Case Number_[0]`, `Case Number_[1]`, cast( `original order` as decimal) a
-
-FROM sakila.attacks 
-order by  cast( `original order` as decimal)a ;
-
-
-/* Compare case studies and dates 
-Normalise date column 
-Check and normalize year column 
-Check for columns with wrong data 
-
-compare the case nuymber in the sakila table and new table to import */ 
-
-
-
+TO DO LIST 
+1. Run previous queries 
+2. Finish the like sharkattacks data cleaning 
+3. Check for href with double http
+4. Check each column one by one 
