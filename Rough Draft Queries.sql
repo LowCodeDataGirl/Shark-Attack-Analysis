11ff7e3922ca513c465f90b590e5c4a29ae61198
LowCodeDataGirl <105794651+LowCodeDataGirl@users.noreply.github.com>
2022-10-12 06:03:53 -0700

    Update Rough Draft Queries.sql

--- Rough Draft Queries.sql
+++ Rough Draft Queries.sql
@@ -1,5 +1,4 @@
 /* 
-
 TO DO LIST 
 1. Run previous queries 
 2. Finish the like sharkattacks data cleaning 
@@ -8,18 +7,47 @@ TO DO LIST
 
 */
 
+
+SELECT  *
+FROM sakila.attacks 
+where `Case Number` ='1758.00.00';
+
 SELECT  *
 FROM sharkattacks.attacks 
+where `Case Number` = '1758.00.00';
+
+SELECT  *
+FROM sakila.attacks 
 where `Case Number_[1]` like '%sharkattack%';
 
 
-SELECT  *
+ /*4f*//* This Query puts the data of ' Alf Dean & other occupant' in the right places */  
+
+/*
+UPDATE 
+    sakila.attacks 
+SET
+    
+    `Date`                   = Concat(Substring(`Case Number`,1,4),'-',Substring(`Case Number`,6,2),'-',Substring(`Case Number`,9,2)),
+    Activity                  = Concat( Activity,'',Injury,'',Species,'',`Investigator or Source`,'',pdf,'',href formula),
+    `Investigator or Source` = `href`,
+    pdf                      = `Case Number_[0]`,
+    `href formula`           = `Case Number_[1]`,
+    href                     =`original order`,
+    `Case Number_[0]`        = Substring(`Case Number_[0]`,1,10),
+    `Case Number_[1]`        = Substring(`Case Number_[0]`,1,10),
+    `original order`         = REPLACE(`original order`,'http://sharkattackfile.net/spreadsheets/pdf_directory/1758.00.00-Mediterranean.pdf',' ')
+   
+WHERE 
+    `Case Number` = '1758.00.00' ;
+
+*/
+
+
+SELECT Concat(Substring(`Case Number`,1,4),'-',Substring(`Case Number`,6,2),'-',Substring(`Case Number`,9,2))
 FROM sharkattacks.attacks 
-where `Case Number` ='1758.00.00' ;
+where `Case Number` = '1758.00.00';
 
+ 
 
-
-  
-
-
-
+ 
